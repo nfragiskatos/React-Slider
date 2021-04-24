01e3c63b0daea8a93c89ec695ffa893000f0d3c4
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-24 15:15:56 -0700

    Logic to control slide position
    - Setup logic to assign the appropriate class to the person article.
    - By default all slides will get the "nextSlide" class
    - If the person we are currently observing has the same index as our state index, then we know it's the active person, so it gets a class of "activeSlide"
    - However if the person's index is 1 less than the current index OR the current index is zero, and the person's index is the last position in the array, then it is considered the previous slide.

--- src/App.js
+++ src/App.js
@@ -13,12 +13,20 @@ function App () {
 					<span>/</span>reviews
 				</h2>
 			</div>
-			<section className="section-center">
+			<div className="section-center">
 				{people.map((person, personIndex) => {
 					const { id, image, name, title, quote } = person;
 					// more functionality here.
+					let position = 'nextSlide';
+					if (personIndex === index) {
+						position = 'activeSlide';
+					}
+
+					if (personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
+						position = 'lastSlide';
+					}
 					return (
-						<article key={id}>
+						<article className={position} key={id}>
 							<img src={image} alt={name} className="person-img" />
 							<h4>{name}</h4>
 							<p className="title">{title}</p>
@@ -33,7 +41,7 @@ function App () {
 				<button className="next">
 					<FiChevronRight />
 				</button>
-			</section>
+			</div>
 		</section>
 	);
 }
