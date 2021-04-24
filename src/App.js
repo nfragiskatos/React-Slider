f2d15c3d008c7a9038b2348854f1893715205cf7
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-24 15:27:22 -0700

    Setup onClick for Next and Prev buttons
    - Could have done it all in the onClick, but decided to use useEffect for something different.
    - Whenever the index gets changed, we want to make sure that it's not out of bounds with the array.
    - useEffect is of course dependent on the index changing, but also need to check the index whenever the array changes in case the size grows or shrinks, therefore changing the out of bounds limits.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ function App () {
 	const [ people, setPeople ] = useState(data);
 	const [ index, setIndex ] = useState(0);
 
+	useEffect(
+		() => {
+			const lastIndex = people.length - 1;
+			if (index < 0) {
+				setIndex(lastIndex);
+			}
+			if (index > lastIndex) {
+				setIndex(0);
+			}
+		},
+		[ index, people ]
+	);
+
 	return (
 		<section className="section">
 			<div className="title">
@@ -35,10 +48,10 @@ function App () {
 						</article>
 					);
 				})}
-				<button className="prev">
+				<button onClick={() => setIndex(index - 1)} className="prev">
 					<FiChevronLeft />
 				</button>
-				<button className="next">
+				<button onClick={() => setIndex(index + 1)} className="next">
 					<FiChevronRight />
 				</button>
 			</div>
